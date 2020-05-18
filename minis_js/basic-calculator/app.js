70f6c611a44e94dc50bb24368799958705803cfe
din <dinndev@gmail.com>
2020-05-18 19:19:11 +0800

    added javascript

---
+++ minis_js/basic-calculator/app.js
@@ -0,0 +1,8 @@
+// SELECTOR
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equallsButton = document.querySelector('[data-equals]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[current-operand]');
+const deleteBtn = document.querySelector('[data-delete]'); 
