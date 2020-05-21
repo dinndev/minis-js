b65c08973c56a6cffc498366b9eea63be87c5103
din <dinndev@gmail.com>
2020-05-21 09:10:26 +0800

    Calculator class added

--- minis_js/basic-calculator/app.js
+++ minis_js/basic-calculator/app.js
@@ -1,3 +1,39 @@
+
+class Calculator {
+    constructor( previousOperandTextElement , currentOperandTextElement) {
+       this.previousOperandTextElement = previousOperandTextElement;
+       this.currentOperandTextElement = currentOperandTextElement;
+    }
+    clear(){
+      const test = this.currentOperand
+     console.log(test)
+    }  
+
+    delete(){
+
+    }
+
+    appendNumber(number){
+
+    }
+   chooseOperation(operation){
+
+   }
+
+   compute(){
+
+   }
+
+   updateDisplay(){
+        
+   }
+}
+
+
+
+
+
+
 // SELECTOR
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
@@ -6,3 +42,6 @@ const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[current-operand]');
 const deleteBtn = document.querySelector('[data-delete]'); 
+
+   
+   
