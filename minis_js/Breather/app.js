ac238c2bcf15e6f5af0d42136593308dfd207f36
din <dinndev@gmail.com>
2020-06-08 19:41:36 +0800

    added javascript

---
+++ minis_js/Breather/app.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container');
+
+const text = document.querySelector('#text');
+
+const totalTime = 7500 ;
+const breatheTime = (totalTime / 5) * 2 ;
+const holdTime = (totalTime / 5);
+
+breathAnimation()
+function breathAnimation(){
+  text.innerText = 'Breath in!'
+  container.className = "container grow"
+   setTimeout(_=> {
+       text.innerText = 'Hold'
+       setTimeout(_=> {
+         text.innerText = 'Breath out'
+       },holdTime)
+   },breatheTime)
+  
+}
+
+
+
+setInterval(breathAnimation , totalTime);
