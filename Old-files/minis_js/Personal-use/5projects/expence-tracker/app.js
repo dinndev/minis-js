ec4920f467cef9a2948aab3b1bcbfe6974d26c93
din <dinndev@gmail.com>
2020-09-15 21:03:32 +0800

    Added the del button

--- Old-files/minis_js/Personal-use/5projects/expence-tracker/app.js
+++ Old-files/minis_js/Personal-use/5projects/expence-tracker/app.js
@@ -2,7 +2,7 @@
 // Selectors 
 const table = document.querySelector('table');
 const inputs = document.querySelectorAll('input');
-const button = document.querySelector('button');
+const button = document.querySelector('[data-key="button"]');
 class Expence {
     constructor(input){
        this.input = input;
@@ -17,16 +17,18 @@ class Expence {
      date,
      amount;
      const row = document.createElement('tr');
+     const delBtn = document.createElement('td');
       inputs.forEach(el => {
         if(el.value === '')return
           switch(el.dataset.key){ //check for the data key and get individual val
-              case "name" : name = el.value;
+              case "name" : name = el.value
               break;
               case "date" : date = el.value;
               break;
-              case "amount" : amount = el.value;
+              case "amount" :amount = el.value;
            }
-          row.innerHTML = `
+        // add the table data in table row
+          row.innerHTML = ` 
           <td>${name}</td>
           <td>${date}</td>
           <td>${amount}</td>
@@ -34,6 +36,17 @@ class Expence {
           el.value = '' //emptying the value
           return table.appendChild(row);
       });
+    //Attach delete button to our row
+      delBtn.innerHTML = `X`
+     delBtn.style.cursor = 'pointer';
+      row.appendChild(delBtn);
+     Array.from(row.children).forEach(el => {
+        if(el.innerHTML.includes('X')){
+            el.addEventListener('click' ,_=> {
+                el.parentElement.remove();
+            })
+        }
+     })
    }
 }
 
