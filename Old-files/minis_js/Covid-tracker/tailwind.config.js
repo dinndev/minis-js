f99cfb7f1ef49f4374596e86bcabb6c8cc425b43
din <dinndev@gmail.com>
2020-09-25 13:58:20 +0800

    Change the border default value

--- Old-files/minis_js/Covid-tracker/tailwind.config.js
+++ Old-files/minis_js/Covid-tracker/tailwind.config.js
@@ -5,10 +5,16 @@ module.exports = {
   },
   purge: [],
   theme: {
+    borderRadius: {
+      'sm': '0.125rem',
+     default: '0.25rem',
+       default: '4px',
+       'md': '0.375rem',
+       'lg': '0.5rem',
+       'full': '60px',
+       'large': '12px'
+    },
     extend: {
-      body: {
-        fontFamily: ['Montserrat']
-      }
     },
   },
   variants: {},
